321813be3396b1199066b5153b4b105a4dd4cb5a
elijah571 <elijahfx43@gmail.com>
2024-07-28 15:48:38 +0100

    weather forecast web application starting files and code

---
+++ weather_forecast/src/App.jsx
@@ -0,0 +1,13 @@
+// import { FaReact } from "react-icons/fa";
+
+function App() {
+  
+
+  return (
+    <div className="text-red-600 text-6xl">App 
+    {/* <h1><FaReact /></h1> */}
+    </div>
+  )
+}
+
+export default App
